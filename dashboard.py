8d0e42028070516ed3d8b29a21874d3842d5896a
dng394 <139503541+dng394@users.noreply.github.com>
2023-08-12 18:16:33 -0700

    first commit

---
+++ dashboard.py
@@ -0,0 +1,24 @@
+import streamlit as st
+import pandas as pd
+import yfinance as yf
+
+st.title('Finance Dashboard') 
+
+tickers = ('TSLA', 'AAPL', 'MSFT', 'GOOGL', 'AMZN', 'BTC-USD', 'ETH-USD')
+dropdown = st.multiselect('Pick assets', tickers)
+start = st.date_input('Start Date', value = pd.to_datetime('2021-01-01'))
+end = st.date_input('End Date', value = pd.to_datetime('today'))
+
+def relativeret(df):
+    rel = df.pct_change()
+    cumret = (1+rel).cumprod() - 1
+
+    # Fill first value for cumulative return
+    cumret = cumret.fillna(0)
+    return cumret
+
+if len(dropdown) > 0:
+    df = relativeret(yf.download(dropdown,start,end)['Adj Close'])
+    st.header('Returns of {}'.format(dropdown))
+
+    st.line_chart(df) 
