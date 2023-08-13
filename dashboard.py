354e4f105af1640a9e625ee78c4dc883d1ac7321
dng394 <139503541+dng394@users.noreply.github.com>
2023-08-12 18:31:26 -0700

    Changed files

--- dashboard.py
+++ dashboard.py
@@ -6,7 +6,7 @@ st.title('Finance Dashboard')
 
 tickers = ('TSLA', 'AAPL', 'MSFT', 'GOOGL', 'AMZN', 'BTC-USD', 'ETH-USD')
 dropdown = st.multiselect('Pick assets', tickers)
-start = st.date_input('Start Date', value = pd.to_datetime('2021-01-01'))
+start = st.date_input('Start Date', value = pd.to_datetime('2020-01-01'))
 end = st.date_input('End Date', value = pd.to_datetime('today'))
 
 def relativeret(df):
@@ -19,6 +19,6 @@ def relativeret(df):
 
 if len(dropdown) > 0:
     df = relativeret(yf.download(dropdown,start,end)['Adj Close'])
-    st.header('Returns of {}'.format(dropdown))
+    st.header('Returns of {}'.format(dropdown)) 
 
     st.line_chart(df) 
